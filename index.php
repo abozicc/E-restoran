c43899bdb26452a1a58ccea8ebe8a890aac4e8cb
ValentinaViskovic <valentina.viskovic@student.fsr.ba>
2017-02-01 23:50:06 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Online restoran</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<section class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div>
+        <div>
+            <a href="vjezba.html" class="navbar-brand">E  -  r e s t o r a n</a>
+        </div>
+        <div>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="vjezba.html">Početna</a></li>
+                <li><a href="#gallery">Galerija</a></li>
+                <li><a href="#menu">Meni</a></li>
+                <li><a href="sign.html">Prijava</a></li>
+                <li><a href="registration/registration.html">Registracija</a></li>
+            </ul>
+        </div>
+    </div>
+</section>
+
+
+<!-- home section -->
+<section id="home">
+    <div class="container">
+        <div class="row">
+            <div>
+                <h1>DOBRODOŠLI NA NAŠ ONLINE RESTORAN</h1>
+                <h2>neka poruka koju ćemo kasnije smislit</h2>
+                <a href="#gallery" class="smoothScroll btn btn-default">G A L E R I J A</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- ponuda -->
+<section id="menu">
+    <div class="container">
+        <div class="row">
+            <h3> DANAŠNJA PONUDA </h3>
+            <hr>
+            <div class="panel panel-back noti-box">
+                <h4>Pizza Vegetale</h4>
+            </div>
+            <div class="row">
+                <h4>Lasagne</h4>
+            </div>
+            <div>
+                <h4>Chickenburger</h4>
+            </div>
+            <div>
+                <h4>Pomfrit</h4>
+            </div>
+            <div>
+                <h4>Girosi</h4>
+            </div>
+            <div>
+                <h4>Makaroni Bolognese</h4>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- kontakt -->
+
+<!-- JAVASCRIPT JS FILES -->
+<script src="lib/jquery.js"></script>
+<!--<script src="js/custom.js"></script>-->
+
+
+</body>
+</html>
